f7bb7a56d3921566c85770400cb7677178d70835
Ophola <zenziophola18@gmail.com>
2026-09-05 16:05:32 +0200

    Create calculator.java

---
+++ __pycache__/calculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new scanner();
+        //input name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter your first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter your second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Enter your third number: ");
+        int num3 = scanner.nextInt();
+
+        System.out.println("Enter your fourth number: ");
+        int num4 = scanner.nextInt();
+
+        System.out.println("Enter your fifth number: ");
+        int num5 = scanner.nextInt();
+
+        int total = num1 + num2 + num3 + num4 + num5;
+        double average = total / 5.0;
+        
+    
+    }
+
+    public static void highest(int highest) {
+        
+        (if )
+
+    }
+}
